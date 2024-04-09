5ae38120757ced500043debf63cb0e71ab00312b
user.name: "Ernesto Lopez <user.email: "chefernestolopez@gmail.com>
2024-04-09 18:56:59 -0400

    add changes materials validations

--- middleware/validator/materialValidator.js
+++ middleware/validator/materialValidator.js
@@ -2,15 +2,30 @@ const { body } = require("express-validator");
 const { validateResult } = require("./validateResult");
 
 exports.validateNewMaterial = [
+  body("userId")
+    .notEmpty()
+    .withMessage("Id del usuario no puede estar vacío")
+    .isString()
+    .withMessage("Id del usuario debe ser un string"),
   body("title")
     .notEmpty()
     .withMessage("Título no puede estar vacío")
     .isString()
     .withMessage("Título debe ser un string"),
-  body("content")
+  body("description")
     .notEmpty()
-    .withMessage("Contenido no puede estar vacío")
+    .withMessage("Descripcion no puede estar vacío")
     .isString()
-    .withMessage("Contenido debe ser un string"),
+    .withMessage("Descripcion debe ser un string"),
+  body("materialURL")
+    .notEmpty()
+    .withMessage("URL de video no puede estar vacío")
+    .isString()
+    .withMessage("URL de video debe ser un string"),
+  body("duration")
+    .notEmpty()
+    .withMessage("Duración no puede estar vacía")
+    .isNumeric()
+    .withMessage("Duración debe ser un numero"),
   validateResult,
 ];
