51480b802a866e072e7e6877b3644f3860c3239b
Felipe <felipe.guzman102@hotmail.com>
2024-04-29 20:20:40 -0400

    fix report foreignKey onDelete

--- models/index.js
+++ models/index.js
@@ -18,7 +18,7 @@ Post.hasMany(Like, { foreignKey: "postId", onDelete: 'CASCADE' });
 User.hasMany(Post, { foreignKey: "userId", as: "posts", onDelete: 'CASCADE' });
 User.hasMany(Resource, { foreignKey: "userId", as: "resources", onDelete: 'CASCADE' });
 User.hasMany(Material, { foreignKey: "userId", as: "materials", onDelete: 'CASCADE' });
-User.hasMany(Report, { foreignKey: "author", as: "authoredReports", onDelete: 'CASCADE' });
+User.hasMany(Report, { foreignKey: "userId", as: "authoredReports", onDelete: 'CASCADE' });
 User.hasMany(Partner, { foreignKey: "userId", as: "partners", onDelete: 'CASCADE' });
 User.hasMany(Like, { foreignKey: "userId", as: "likes", onDelete: 'CASCADE' });
 
